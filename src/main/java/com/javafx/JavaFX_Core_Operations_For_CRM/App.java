97dec9a7a102b39d47407cf02dfa8c749e866136
Nikhil-197 <mahajannikhil177@gmail.com>
2024-02-04 12:48:01 +0530

    First Commit by NM

---
+++ src/main/java/com/javafx/JavaFX_Core_Operations_For_CRM/App.java
@@ -0,0 +1,13 @@
+package com.javafx.JavaFX_Core_Operations_For_CRM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
